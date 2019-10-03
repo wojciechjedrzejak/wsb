8a721c62543183f1aec2069ace9239dfb3f8d0f8
wojciechjedrzejak <wojciechjedrzejak99@gmail.com>
2019-10-03 12:09:00 +0200

    first c#

---
+++ podstawy_programowania/zajęcia1/pierwszy_plik/pierwszy_plik/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pierwszy_plik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WSB");
+            Console.WriteLine("Nowa linia");
+
+            Console.Write("Proszę podaj swoje imię: ");
+            //typ string
+            string name = Console.ReadLine();
+            Console.Write("Masz na imię: ");
+            Console.WriteLine(name);
+
+            //typ integer
+            int age = 20;
+            Console.WriteLine(age);
+
+            uint number = 10;
+
+            //typ float
+            float z = 14.55F;
+            //+ konkatenacja
+            Console.WriteLine("Zmienna z wynosi: " + z);
+
+            Console.WriteLine("Program obliczający pole prostokąta");
+            float a = float.Parse(Console.ReadLine());
+            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Pole prostokąta jest równe: " + a * b);
+
+
+            int j, k = 10, l;
+
+            //int zmienna = 10;
+            int zmienna = new int();
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
