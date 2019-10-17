402bc7c0a7578aa78ef7cf44decb0d8ba78c4098
wojciechjedrzejak <56110889+wojciechjedrzejak@users.noreply.github.com>
2019-10-17 12:11:18 +0200

    Add files via upload
    xd

---
+++ podstawy_programowania/zajecia3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * napisz program, ktory za pomocą instrukcji do while dla danych wartości zmieniających się w przedziale od 0 do 15 oblicza wartość funkcji y = 4x
+             */
+            int y = 0, x = 0;
+            do
+            {
+                y = 4 * x;
+                Console.WriteLine("x = {0}\ty = {1}", x, y);
+                x++;
+            } while (x <= 15);
+
+            //napisz program znajduący się powyżej za pomocą pętli while
+
+            x = 0;
+            y = 0;
+
+            while(x <= 15)
+            {
+                y = 4 * x;
+                Console.WriteLine("x = {0}\ty = {1}", x, y);
+                x++;
+            }
+
+            //napisz program, który za pomocą instrukcji while dla danych wartości x zmieniających się w przedziale od 1 do 50 obliczy ich sumę
+            Console.WriteLine();
+            x = 1; 
+            int suma = 0;
+
+            while(x <= 50)
+            {
+                suma = suma + x;
+                x++;
+            }
+            Console.WriteLine("suma = {0}", suma);
+
+            //wypisz tabliczkę mnożenia
+            int iloczyn;
+            for(int i=1; i<=10; i++)
+            {
+                for(int j=1; j<=10; j++)
+                {
+                    iloczyn = i * j;
+                    Console.Write("{0}\t", iloczyn);
+                }
+                Console.WriteLine();
+            }
+
+            //napisz program, który za pomocą pentli for zsumuje liczby nieparzyste z przedziału od 1 do 10
+            int c = 0;
+            for(int i=1; i<=10; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    c += i;
+                }
+            }
+            Console.WriteLine("suma = {0}", c);
+
+            int n = 1, m = 1;
+            int iloczyn2 = 0;
+            do
+            {
+                do
+                {
+                    iloczyn2 = n * m;
+                    Console.Write("{0}\t", iloczyn2);
+                    m++;
+                } while (m <= 10);
+                n++;
+                m = 1;
+                Console.WriteLine();
+            } while (n <= 10);
+
+            //Napisz program, który wyświetli duże litery alfabetu od A do Z i od Z do A z wykorzystaniem pętli for
+
+            char znak = 'A';
+            for(znak='A'; znak<'Z'; znak++)
+            {
+                Console.Write("{0}, ", znak);
+            }
+            Console.Write("Z.");
+            Console.WriteLine();
+            for(znak='Z'; znak>'A'; znak--)
+            {
+                Console.Write("{0}, ", znak);
+            }
+            Console.Write("A. ");
+
+            int j = 1;
+            for (; j <= 10; j++)
+            {
+                if (j == 3 || j == 6 || j == 7)
+                    continue;
+                Console.Write(j + ' ');
+            }
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ podstawy_programowania/zajecia3/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisz program, który w 10-elementowej tablicy jednowymiarowej o nazwie tab umieszcza liczby całkowite z przedziału od 1 do 10
+
+            int n = 10, i;
+            //deklaracja tablicy o nazwie tab typu całkowitego o rozmiarze n = 10
+            int[] tab = new int[n];
+
+            for (i = 0; i < tab.Length; i++)
+            {
+                tab[i] = i;
+            }
+
+            //wyświetlenie zawartości tablicy
+
+            for (i = 0; i < tab.Length; i++)
+            {
+                Console.WriteLine(tab[i]);
+            }
+
+            /*napisz program, który w 5-elementowej tablicy jednowymiarowej o nazwie colors umieszcza nazwy kolorów podane z klawiatury przez użytkownika. Wyświetl kolory w formacie
+             * kolor1: red
+             * kolor2: blue
+             * ...
+             * kolor5: yellow
+             */
+            string[] colors = new string[5];
+            string color;
+            for (i = 0; i < colors.Length; i++)
+            {
+                Console.Write("Podaj ulubiony kolor:");
+                color = Console.ReadLine();
+                colors[i] = color;
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Ulubione kolory: ");
+            int x = 0, count;
+            while (x < colors.Length)
+            {
+                count = x + 1;
+                Console.WriteLine("Kolor{0}:{1}", count, colors[x]);
+                x++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
