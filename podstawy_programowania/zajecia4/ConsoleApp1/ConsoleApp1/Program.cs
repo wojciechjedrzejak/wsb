245671368ab1897b0d8393facdb50e4ac09de213
wojciechjedrzejak <56110889+wojciechjedrzejak@users.noreply.github.com>
2019-10-24 12:07:29 +0200

    Add files via upload
    h

---
+++ podstawy_programowania/zajecia4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.Write(tab[i] + " ");
+            }
+
+            foreach (int value in tab)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine("\n");
+            
+            int j = 0;
+            int size = tab.Length;
+            size--;
+            foreach (int value in tab)
+            {
+                if (j == 0)
+                {
+                    Console.WriteLine("Pierwszy element: {0}", tab[0]);
+                }
+                else if (j == size)
+                {
+                    Console.WriteLine("Ostatni element: {0}", tab[size]);
+                }
+                else
+                {
+                    Console.WriteLine("Kolejny element: {0}", tab[j]);
+                }
+                j++;
+            }
+            Console.WriteLine();
+            /*utwórz tablicę n elementową, której rozmiar podaje użytkownik z klawiatury użytkownik następnie podaje ulubione kolory z klawiatury
+             * wyświetl kolory w formacie
+             * Kolor1: ...
+             * Kolor2: ...
+             */
+            string text;
+            byte count;
+
+            do
+            {
+                Console.Write("Ilość ulubionych kolorów:");
+                text = Console.ReadLine();
+            } while (byte.TryParse(text, out count) == false);
+
+            string[] colors = new string[count];
+
+            //Console.WriteLine("\nIlość ulubionych kolorów: {0}", colors.Length);
+
+            //count || colors.Length
+            string color;
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("Podaj kolor: ");
+                color = Console.ReadLine();
+                colors[i] = color;
+            }
+            int h = 0, y;
+            while (h < colors.Length)
+            {
+                y = h;
+                y++;
+                Console.WriteLine("Kolor{0}: {1}", y, colors[h]);
+                h++;
+            }
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ podstawy_programowania/zajecia4/tablice_wielowymiarowe/tablice_wielowymiarowe/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tablice_wielowymiarowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tablice prostokątne
+
+            int[,] tab = new int[,]
+            {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+            };
+
+
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                //wiersze
+                for (int j= 0; j < tab.GetLength(1); j++)
+                {
+                    //kolumny
+                    Console.Write("{0}\t", tab[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            //Utwórz tablicę dwuwymiarową i zainicjuj ją danymi i * 3 + j
+
+            int[,] tab2 = new int[3, 3];
+
+            for (int i = 0; i < tab2.GetLength(0); i++)
+            {
+                for (int j = 0; j < tab2.GetLength(1); j++)
+                {
+                    tab2[i, j] = i * 3 + j;
+                    Console.Write("{0}\t", tab2[i, j]);
+                }
+                Console.WriteLine("\n");
+
+            }
+
+            string[][] country = new string[4][];
+
+            Console.WriteLine("Rozmiar zewnętrznego wymiaru tablicy nieregularnej {0}", country.Length);
+
+            int[][] numbers2 = new int[3][]
+            {
+                new int[] {1},
+                new int[] {2, 3, 4, 5},
+                new int[] {6, 7},
+            };
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int i = 0; i < length; i++)
+                {
+
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
